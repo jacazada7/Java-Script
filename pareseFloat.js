07f62bf02ca439e173cac746489f8b852678ef26
jacazada7 <joaoalvves028@gmail.com>
2024-05-07 08:45:50 -0400

    parese add

---
+++ pareseFloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.60";
+console.log(stringParaDecimal); // Saída "12.60"
+console.log(typeof(stringParaDecimal)); // Saída: string
+console.log(""); // Saída: "12.60"
+//Conversão da String "stringaParaDecimal" para Decimal
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal); //Saída: 12.60
+console.log(typeof(decimal)); // Saída: number
