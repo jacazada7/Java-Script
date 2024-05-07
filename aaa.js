897b715c6c2e10598088c3ea704a83125b248334
jacazada7 <joaoalvves028@gmail.com>
2024-05-07 08:45:11 -0400

    aaa add

---
+++ aaa.js
@@ -0,0 +1,4 @@
+let nome = "Jaca";
+let idade = 17;
+let saudacao = "Iae eu sou trem bala de Cuiabá" + nome + "e eu tenho" + idade + "anos.";
+console.log(saudacao) //Saída: "iae eu sou jaca trem bala de Cuiabá e eu tenho 17 anos"
