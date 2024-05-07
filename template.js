5cc7ef2aa83562ab1a73b213be90509476f29bdb
jacazada7 <joaoalvves028@gmail.com>
2024-05-07 08:43:35 -0400

    template add

---
+++ template.js
@@ -0,0 +1,6 @@
+let nome = "João Gabriel";
+let idade = 17;
+
+//Usando template literals para criar uma string com váriaveis
+let saudacao = 'Olá, meu nome é ${nome} e eu tenho ${idade} anos.';
+console.log(saudacao); // Saída: "Olá, meu nome é João Gabriel e eu tenho 17 anos"
