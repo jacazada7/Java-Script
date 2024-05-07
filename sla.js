033815e4882e3e7641dffdb512c0306e2dce7158
jacazada7 <joaoalvves028@gmail.com>
2024-05-07 08:41:20 -0400

    sla add

---
+++ sla.js
@@ -0,0 +1,6 @@
+let concatenacao = "1.1" + "1.1";
+let converterParaNumero = +"1.1";
+console.log(typeof(concatenacao)); //"1.11.1"
+console.log("");
+console.log(converterParaNumero); //1.1
+console.log(typeof(converterParaNumero)); // number
