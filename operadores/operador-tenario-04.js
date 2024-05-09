0883fe4aff86289168637d00c3602a3e0fcaa9a6
jacazada7 <joaoalvves028@gmail.com>
2024-05-09 10:14:59 -0400

    04/2 add

---
+++ operadores/operador-tenario-04.js
@@ -0,0 +1,4 @@
+const firstCheck = false;
+const secondCheck = false;
+const acces = firstCheck ? "Acesso negado" : secondCheck ? "Acesso negado" : "Acesso concedido";
+console.log(acces); // Saída: "Acesso concedido"
