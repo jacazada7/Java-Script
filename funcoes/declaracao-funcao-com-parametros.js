deb3ebcc34bbf92cc58b178e44be407b6bc9d3a5
jacazada7 <joaoalvves028@gmail.com>
2024-05-14 11:17:29 -0400

    funcao com

---
+++ funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro1, parametro2) {
+    // corpo da função
+    // Pode conter uma ou mais instruções
+    return resultado; // Opcional
+}
+ // Chaamada da função
+ nomeDaFuncao(parametro1, parametro2);
